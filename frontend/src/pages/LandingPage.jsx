a230269467e4c7f2532bc99079121199d626a4f8
aryansh-2003 <dixitsanjeev1975@gmail.com>
2026-04-30 22:37:33 +0530

    Fixed layout issue

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 import { Fonts } from '../components/ui/Fonts';
 import { NeuralCanvas } from '../components/ui/NeuralCanvas';
 
-
 import Nav  from '../Navbar/Navbar';
 import { Footer } from '../components/layout/Footer';
-
 
 import { HeroSection } from '../components/sections/HeroSection';
 import { MarqueeTicker } from '../components/sections/MarqueeTicker';
@@ -18,12 +16,16 @@ import { CTASection } from '../components/sections/CTASections';
 
 export default function LandingPage() {
   return (
-    <div style={{ minHeight: '100vh', background: '#020510', color: '#fff', overflowX: 'hidden', fontFamily: "'Space Grotesk', sans-serif" }}>
-      <Fonts />
+    <div style={{ minHeight: '100vh', background: '#020510', color: '#fff', overflowX: 'hidden', fontFamily: "'Space Grotesk', sans-serif" }} 
+         className="flex flex-col relative w-full">
+      
       <NeuralCanvas />
       
-      <div style={{ position: 'relative', zIndex: 10 }}>
+      <div className="relative w-full z-50 pt-1 shrink-0">
         <Nav />
+      </div>
+
+      <main className="relative z-10 w-full flex flex-col flex-1 mt-4 sm:mt-12">
         <HeroSection />
         <MarqueeTicker />
         <LiveDemoSection />
@@ -32,7 +34,8 @@ export default function LandingPage() {
         <SettingsSection />
         <CTASection />
         <Footer />
-      </div>
+      </main>
+      
     </div>
   );
 }
